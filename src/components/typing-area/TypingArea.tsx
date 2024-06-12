2b9f0664e97c7cdb87fad057a1d65ee97287a824
vnilla <kiejuc19@htl-kaindorf.ac.at>
2024-06-12 10:10:08 +0200

    + updated 'space', 'backspace' interaction

--- src/components/typing-area/TypingArea.tsx
+++ src/components/typing-area/TypingArea.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, {useEffect, useState, useRef, useCallback} from 'react';
 import dynamic from "next/dynamic";
 
 type TypingAreaProps = {
@@ -8,7 +8,7 @@ type TypingAreaProps = {
     isTypingDisabled: boolean;
 };
 
-const TypingArea = ({ onTypingStart, isTypingDisabled }: TypingAreaProps) => {
+const TypingArea = ({onTypingStart, isTypingDisabled}: TypingAreaProps) => {
 
     const GLOBAL_TIMER = 30;
     const [timer, setTimer] = useState(GLOBAL_TIMER);
@@ -62,101 +62,161 @@ const TypingArea = ({ onTypingStart, isTypingDisabled }: TypingAreaProps) => {
     }, [addClass, formatWord, getRandomWord]);
 
     const handleKeyDown = useCallback((e: KeyboardEvent) => {
-        if (isTypingDisabled) return;
+                if (isTypingDisabled) return;
 
-        onTypingStart();
+                // onTypingStart();
 
-        const key = e.key;
-        const currentWord = document.querySelector('.word.current') as HTMLElement;
-        const currentLetter = document.querySelector('.letter.current') as HTMLElement;
-        const expected = currentLetter?.innerHTML || ' ';
+                const key = e.key;
+                const currentWord = document.querySelector('.word.current') as HTMLElement;
+                const currentLetter = document.querySelector('.letter.current') as HTMLElement;
+                const expected = currentLetter?.innerHTML || ' ';
 
-        if (!currentWord) return;
+                if (!currentWord) return;
 
-        const isLetter = key.length === 1 && key !== ' ';
-        const isSpace = key === ' ';
-        const isBackspace = key === 'Backspace';
-        const isFirstLetter = currentLetter === currentWord.firstChild;
+                const isLetter = key.length === 1 && key !== ' ';
+                const isSpace = key === ' ';
+                const isBackspace = key === 'Backspace';
+                const isFirstLetter = currentLetter === currentWord.firstChild;
 
-        if (isLetter) {
-            if (currentLetter) {
-                addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
-                removeClass(currentLetter, 'current');
-                if (currentLetter.nextSibling) {
-                    addClass(currentLetter.nextSibling as HTMLElement, 'current');
-                }
-            } else {
-                const incorrectLetters = document.createElement('span');
-                incorrectLetters.innerHTML = key;
-                incorrectLetters.className = 'letter incorrect extra';
-                currentWord.appendChild(incorrectLetters);
-            }
-        }
-
-        if (isSpace) {
-            // Invalidate the current word if there are any incorrect letters
-            const lettersToInvalidate = Array.from(currentWord.querySelectorAll('.letter:not(.correct)')) as HTMLElement[];
-            if (lettersToInvalidate.length > 0) {
-                lettersToInvalidate.forEach(letter => {
-                    addClass(letter, 'incorrect');
-                });
-            }
-            removeClass(currentWord, 'current');
-            if (currentWord.nextSibling) {
-                addClass(currentWord.nextSibling as HTMLElement, 'current');
-                if (currentWord.nextSibling.firstChild) {
-                    addClass(currentWord.nextSibling.firstChild as HTMLElement, 'current');
-                }
-            }
-        }
-
-        if (isBackspace) {
-            if (currentLetter && isFirstLetter) {
-                removeClass(currentWord, 'current');
-                if (currentWord.previousSibling) {
-                    addClass(currentWord.previousSibling as HTMLElement, 'current');
-                    const lastChild = currentWord.previousSibling.lastChild as HTMLElement;
-                    if (lastChild) {
-                        addClass(lastChild, 'current');
-                        removeClass(lastChild, 'incorrect');
-                        removeClass(lastChild, 'correct');
+                if (isLetter) {
+                    if (currentLetter) {
+                        addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+                        removeClass(currentLetter, 'current');
+                        if (currentLetter.nextSibling) {
+                            addClass(currentLetter.nextSibling as HTMLElement, 'current');
+                        }
+                    } else {
+                        const incorrectLetters = document.createElement('span');
+                        incorrectLetters.innerHTML = key;
+                        incorrectLetters.className = 'letter incorrect extra';
+                        currentWord.appendChild(incorrectLetters);
                     }
                 }
-            } else if (currentLetter && !isFirstLetter) {
-                removeClass(currentLetter, 'current');
-                addClass(currentLetter.previousSibling as HTMLElement, 'current');
-                removeClass(currentLetter.previousSibling as HTMLElement, 'incorrect');
-                removeClass(currentLetter.previousSibling as HTMLElement, 'correct');
-            } else {
-                const lastChild = currentWord.lastChild as HTMLElement;
-                if (lastChild) {
-                    addClass(lastChild, 'current');
-                    removeClass(lastChild, 'incorrect');
-                    removeClass(lastChild, 'correct');
-                    const extraLetters = currentWord.querySelectorAll('.letter.extra') as NodeListOf<HTMLElement>;
-                    if (extraLetters.length > 0) {
-                        extraLetters[extraLetters.length - 1].remove();
+
+                if (isSpace) {
+                    // check if 'space' is expected
+                    if (expected === ' ') {
+                        // 'space' is expected, move to the next word
+                        removeClass(currentWord, 'current');
+
+                        if (currentWord.nextSibling) {
+                            addClass(currentWord.nextSibling as HTMLElement, 'current');
+
+                            const firstChild = currentWord.nextSibling.firstChild as HTMLElement;
+                            if (firstChild) {
+                                addClass(firstChild, 'current');
+                            }
+                        }
+                    } else {
+                        // 'space' is not expected (i.e. in the middle of a word)
+                        // invalidate rest of the letters
+                        const lettersToInvalidate = Array.from(currentWord.querySelectorAll('.letter:not(.correct)')) as HTMLElement[];
+                        if (lettersToInvalidate.length > 0) {
+                            lettersToInvalidate.forEach(letter => {
+                                addClass(letter, 'incorrect');
+                            });
+                        }
+
+                        // remove current class from the current letter & word
+                        removeClass(currentLetter, 'current');
+                        removeClass(currentWord, 'current');
+
+                        // add current class to the next word & first letter
+                        if (currentWord.nextSibling) {
+                            addClass(currentWord
+                                .nextSibling as HTMLElement, 'current');
+                            if (currentWord.nextSibling.firstChild) {
+                                addClass(currentWord
+                                    .nextSibling.firstChild as HTMLElement, 'current');
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        if (currentWord.getBoundingClientRect().top > 200) {
-            const wordsDiv = wordsDivRef.current;
-            if (wordsDiv) {
-                const margin = parseInt(wordsDiv.style.marginTop || '0');
-                wordsDiv.style.marginTop = (margin - 35) + 'px';
-            }
-        }
+                // check for backspace
+                if (isBackspace) {
+                    // check if not 'backspace' is expected
+                    const isFirstWord = currentWord === wordsDivRef.current?.firstChild;
+                    if (isFirstWord && isFirstLetter) return;
 
-        const nextLetter = document.querySelector('.letter.current');
-        const nextWord = document.querySelector('.word.current');
-        const cursor = cursorRef.current;
-        if (cursor) {
-            cursor.style.top = (nextLetter || nextWord)!.getBoundingClientRect().top + 3 + 'px';
-            cursor.style.left = (nextLetter || nextWord)!.getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
-        }
-    }, [addClass, onTypingStart, removeClass, isTypingDisabled]);
+                    // if 'backspace' is pressed on the first letter of the word, remove 'current' from word and letter, move to the previous word
+                    if (currentLetter && isFirstLetter) {
+                        removeClass(currentWord, 'current');
+                        removeClass(currentLetter, 'current');
+
+                        // move to previous word
+                        if (currentWord.previousSibling) {
+                            // add 'current' to the previous word
+                            addClass(currentWord.previousSibling as HTMLElement, 'current');
+
+                            // add 'current' to the last letter of the previous word
+                            const lastChild = currentWord.previousSibling.lastChild as HTMLElement;
+                            if (lastChild) {
+                                addClass(lastChild, 'current');
+
+                                // remove 'correct' and 'incorrect' classes
+                                removeClass(lastChild, 'incorrect');
+                                removeClass(lastChild, 'correct');
+                            }
+                        }
+                    }
+
+
+                    // if 'backspace' is pressed on a letter other than the first letter of the word, move to the previous letter
+                    if (currentLetter && !isFirstLetter) {
+                        // remove 'current' class from the current letter
+                        removeClass(currentLetter, 'current');
+
+                        // add 'current' to the previous letter
+                        addClass(currentLetter.previousSibling as HTMLElement, 'current');
+
+                        // remove 'correct' and 'incorrect' classes
+                        removeClass(currentLetter.previousSibling as HTMLElement, 'incorrect');
+                        removeClass(currentLetter.previousSibling as HTMLElement, 'correct');
+                    }
+
+                    // if 'backspace' is pressed on the last character of the word
+                    if (!currentLetter) {
+                        // add 'current' to the last letter of the previous word
+                        const lastChild = currentWord.lastChild as HTMLElement;
+                        if (lastChild) {
+                            addClass(lastChild, 'current');
+
+                            // remove 'correct' and 'incorrect' classes
+                            removeClass(lastChild, 'incorrect');
+                            removeClass(lastChild, 'correct');
+                        }
+                    }
+
+                    if (currentWord) {
+                        // remove extra letters
+                        const extraLetters = currentWord.querySelectorAll('.letter.extra') as NodeListOf<HTMLElement>;
+                        if (extraLetters.length > 0) {
+                            extraLetters[extraLetters.length - 1].remove();
+                        }
+                    }
+                }
+
+                if (currentWord.getBoundingClientRect().top > 200) {
+                    const wordsDiv = wordsDivRef.current;
+                    if (wordsDiv) {
+                        const margin = parseInt(wordsDiv.style.marginTop || '0');
+                        wordsDiv.style.marginTop = (margin - 35) + 'px';
+                    }
+                }
+
+                const nextLetter = document.querySelector('.letter.current');
+                const nextWord = document.querySelector('.word.current');
+                const cursor = cursorRef.current;
+                if (cursor) {
+                    cursor.style.top = (nextLetter || nextWord)!.getBoundingClientRect().top + 3 + 'px';
+                    cursor.style.left = (nextLetter || nextWord)!.getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+                }
+            }
+            ,
+            [addClass, /*onTypingStart,*/ removeClass, isTypingDisabled]
+        )
+    ;
 
     useEffect(() => {
         newGame();
@@ -169,6 +229,10 @@ const TypingArea = ({ onTypingStart, isTypingDisabled }: TypingAreaProps) => {
         }
     }, [handleKeyDown, newGame]);
 
+    useEffect(() => {
+        onTypingStart();
+    }, [onTypingStart]);
+
     return (
         <>
             <div id="game" ref={gameDivRef} tabIndex={0}>
@@ -180,4 +244,4 @@ const TypingArea = ({ onTypingStart, isTypingDisabled }: TypingAreaProps) => {
     );
 };
 
-export default dynamic(() => Promise.resolve(TypingArea), { ssr: false });
+export default dynamic(() => Promise.resolve(TypingArea), {ssr: false});
